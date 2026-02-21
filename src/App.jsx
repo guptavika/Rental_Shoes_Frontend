10dc1ed7fb22efb6a398ca9e7654a2bd49caea2a
Vikash <vikahsgupta2226@gmial.com>
2026-02-21 13:48:12 +0530

    C

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
-// import Login from "./pages/Login";
+ 
 import Admin from "./pages/Admin";
 import User from "./pages/User";
 import Login from "./pages/Login";
+import AdminLayout from "./pages/AdminLayout";
+import Dashboard from "./pages/Dashboard";
+import ManageShoes from "./pages/ManagesShoes";
+import Booking from "./pages/Booking";
+import Reports from "./pages/Report";
+
 // import { Login } from "@mui/icons-material";
 // import Login from "./pages/Login";
 
@@ -17,7 +23,17 @@ export default function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/admin" element={<Admin/>}/>
         <Route path="/user" element={<User/>}/>
+          <Route path="/adminlayout" element={<AdminLayout/>}>
+          <Route path="dashboard" element={<Dashboard />} />
+          {/* <Route path="add-shoes" element={<AddShoe />} /> */}
+          <Route path="manage-shoes" element={<ManageShoes/>} />
+          <Route path="bookings" element={<Booking />} />
+          <Route path="users" element={<User />} />
+
+          <Route path="reports" element={<Reports />} />
+        </Route>
       </Routes>
     </BrowserRouter>
+
   );
 }
